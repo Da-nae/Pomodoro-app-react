b42f00c9f6d71bd479b6f344f133e37588bed2d6
Da-nae <grosjean.danae@gmail.com>
2022-11-25 12:21:40 +0100

    base of timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Timer from './components/Timer.jsx';
+import { useState } from 'react';
 
 function App() {
+    const [secs, setSecs] = useState(25 * 60);
 
     return (
         <div className="App">
-            <Timer />
+            <Timer secs={secs} />
         </div>
     )
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,17 +1,73 @@
-import { useStopwatch } from 'react-timer-hook';
 import { VscDebugRestart } from 'react-icons/vsc';
 
-const Timer = () => {
-    const {seconds, minutes, isRunning, start, pause, reset,} = useStopwatch({ autoStart: false });
+function Timer({secs}){
+    //const [buttonText, setButtonText] = useState('Start');
+const Time = (secs) => {
+        let hours = Math.floor(secs / (60 * 60));
+    
+        let divisor_for_minutes = secs % (60 * 60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
+    
+        let divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+    
+        let time = {
+            h: hours,
+            m: minutes,
+            s: seconds
+        };
+
+        let hour = time.h;
+        let min = time.m;
+        let second = time.s;
+
+        console.log(time.h, time.m, time.s);
+
+        if(time.m < 10){
+            min = `0`+ time.m;
+        }
+
+        if(time.s < 10){
+            second = `0`+ time.s;        
+        }
+
+        let timeDisplay = `0${hour}:${min}:${second}`;
+
+        if(time.h == 0){
+            return timeDisplay = `${min}:${second}`;
+        }
+
+
+        return timeDisplay;
+    }
+
+
+    // function Reset() {
+    //     console.log('reset');
+    //     return;
+    // }
+
+    // function startPause() {
+    //     if(){
+    //         pause();
+    //         setButtonText('Pause');
+    //     } else {
+    //         start();
+    //     }
+    // }
+
+    // function startPause() {
+    //     setButtonText('Pause');
+    //     return;
+    // }
 
     return (
         <div className="flex justify-center">
             <div className="block p-6 rounded-lg shadow-lg  bg-dark-salmon mt-10">
                 <button type="button" className="btnTop text-bisque text-xl leading-tight font-medium mb-2 text-center">Work timer</button>
                 <div className='flex justify-around items-center mb-4'>
-                    <p className="text-txtgrey text-center text-lg">
-                        {minutes}:{seconds}
-                        
+                    <p className="text-txtgrey text-center text-3xl">
+                        {Time(secs)}
                     </p>
                     <div className='btnPlus'>
                         <button className='plus'>+</button>
@@ -19,8 +75,9 @@ const Timer = () => {
                     </div>
                 </div>
                 <div className='flex'>
-                    <button type="button" onClick={isRunning ? pause : start} className="startTimer text-txtgrey btnStart inline-block">Start</button>
-                    <button type="button" className='text-bisque mx-2' onClick={reset}><VscDebugRestart /></button>
+                    <button type="button" 
+                    className="startTimer text-txtgrey btnStart inline-block">Start</button>
+                    <button type="button" className='text-bisque mx-2'><VscDebugRestart /></button>
                 </div>
             </div>
         </div>
