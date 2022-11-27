b180d15e8341c39475680aa2c91b6a2ba1733cd2
Da-nae <grosjean.danae@gmail.com>
2022-11-27 13:48:36 +0100

    test modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import "./App.css";
 import React from "react";
 import Timer from "./components/Timer.jsx";
 import {useState} from "react";
+// test 
+import useModal from "./components/useModal";
+import Modal from "./components/modal";
 
 function App() {
     const [secs, setSecs] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
+    const { isShowing, toggle } = useModal();
 
     // Button to add a minute to the timer :
     const BtnPlus = () => {
@@ -25,6 +29,28 @@ function App() {
         }
     };
 
+    // Button to switch from work to break : 
+    const Breaktime = () => {
+            if (!isActive) {
+                setSecs(5 * 60);
+            }
+        };
+
+    // Button to switch from break to work : 
+    const Worktime = () => {
+        if (!isActive) {
+            setSecs(25 * 60);
+        }
+    };
+
+    // Modal test
+    // const useModalfunction = () => {
+    //     if(secs == 0) {
+    //         isShowing();
+    //     }
+    // }
+    // useModalfunction();
+
     return (
         <div className="App">
             <div className="flex justify-center">
@@ -32,12 +58,12 @@ function App() {
                     <div className="btnsTop flex">
                         <button
                             type="button"
-                            className="btnTop text-bisque text-xl leading-tight font-medium mb-2 text-center">
+                            className="btnTop text-bisque text-xl leading-tight font-medium mb-2 text-center" onClick={Worktime}>
                             Work timer
                         </button>
                         <button
                             type="button"
-                            className="btnTop text-dark-salmon bg-bisque text-xl leading-tight font-medium mb-2 text-center">
+                            className="btnTop text-dark-salmon bg-bisque text-xl leading-tight font-medium mb-2 text-center" onClick={Breaktime}>
                             Breaktime
                         </button>
                     </div>
@@ -49,6 +75,10 @@ function App() {
                         BtnPlus={BtnPlus}
                         BtnMinus={BtnMinus}
                     />
+                        {/* <button className="modal-toggle" onClick={toggle}>
+                        Show modal
+                        </button>
+                        <Modal isShowing={isShowing} hide={toggle} /> */}
                 </div>
             </div>
         </div>
